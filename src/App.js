6209047d76192a4a34e8ab8563f8a1f9c33edc71
abblaya <72778901+abblaya@users.noreply.github.com>
2020-10-13 15:04:34 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           MASTER FINAL
+           MASTER FINAL 2
           </p>
           <a
             className="App-link"
